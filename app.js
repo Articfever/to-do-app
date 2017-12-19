350fbc0571af1412f101538964360650238605b3
rae <31858931+Articfever@users.noreply.github.com>
2017-12-19 09:54:35 -0800

    delete button

---
+++ app.js
@@ -0,0 +1,45 @@
+funtion onReady() {
+const addToDoForm = document.getElementById('addToDoForm');
+const newToDoText = document.getElementById('newToDoText');
+const toDoList = document.getElementById('toDoList');
+
+addToForm.addEventListener();
+addToForm.addEventListener('submit', () => {
+  form.addEventListener( 'submit', function( ev ) {
+    var text = field.value;
+    if ( text !== '' ) {
+      todo.innerHTML += '<li>' + text + ' <button onclick="Delete(this);">Delete</button> </li>';
+      field.value = '';
+event.preventDefailt();
+
+//get the text
+let title = newToDoText.value;
+
+//create a new li
+let newLi = document.createElement('li');
+
+//create a new input
+let checkbox = document.create.Element('input');
+
+//set the input's type to check box
+checkbox.type = "checkbox";
+
+//set the title
+newLi.textContent = title;
+
+//attach the checkbox to the li
+newLi.appendChild(checkbox);
+
+//attach the li to the ul
+toDoList.appendChild(newLi);
+
+//empty the input
+newToDoText = '';
+
+});
+}
+}
+window.onload = function() {
+  alert("The window has loaded!");
+  onReady();
+};
