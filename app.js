bdfe14c6a106a0030acbee891ef6e849529c6d1a
rae <31858931+Articfever@users.noreply.github.com>
2017-12-19 13:32:54 -0800

    checkpoint 8 UI

--- app.js
+++ app.js
@@ -39,6 +39,54 @@ newToDoText = '';
 });
 }
 }
+
+function onReady () {
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  function createNewToDo(){
+    if (newToDoText.value) { return; }
+    
+    toDos.push({
+      title: nextToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = ''
+
+    renderTheUI();
+  }
+
+    function renderTheUI() {
+      const toDoList = document.getElementById('toDoList');
+
+      toDoList.textContent = '';
+
+      toDos.forEach(function(toDo) {
+        const newToDo = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        newLi.textContent = toDo.title;
+
+        toDoList.appendChild(newli);
+        newLi.appendChild(checkbox);
+
+        const title = document.createElement('span');
+        title.textContent = toDo.title;
+
+      });
+    }
+    addToDoForm.addEventListener('submit', event => {
+      event.preventDefault();
+      createNewToDo();
+      newToDoText.value = '';
+    });
+  }
+  renderTheUi();
+}
 window.onload = function() {
   alert("The window has loaded!");
   onReady();
